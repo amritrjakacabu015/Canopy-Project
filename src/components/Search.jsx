0f67802d68c51bf93fd8dfc3369f7880a9cd8c50
Amritraj <amritrjakacabu015@gmail.com>
2018-12-02 23:54:05 -0500

    styling the index page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
+import "../App.css";
 class Search extends Component {
   render() {
     return (
       <React.Fragment>
         <form>
-          <h1>Search Employee</h1>
+          <h1>
+            <center>Search Employee</center>
+          </h1>
 
-          <input type="text" placeholder="Search" />
+          <center>
+            <input className="Search-Box" type="text" placeholder="Search" />
 
-          <select>
-            <option value="SSN">By SSN</option>
-            <option value="EID">By Employee Number</option>
-          </select>
+            <select>
+              <option value="SSN">By SSN</option>
+              <option value="EID">By Employee Number</option>
+            </select>
 
-          <button>Search</button>
+            <button>Search</button>
+          </center>
         </form>
       </React.Fragment>
     );
